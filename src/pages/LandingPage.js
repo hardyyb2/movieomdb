8481bef8e23de058358f6e812954a745c402ade9
hardyyb3 <heartz.sd3@gmail.com>
2020-03-27 19:45:10 +0530

    lading page prob solved

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -19,61 +19,71 @@ const LandingPage = props => {
     const [signup, setSignup] = useState(false)
     const classes = useStyles()
 
-    return (
-        <>
-            <Grid container justify="center" className={classes.root}
+    const landing = (<>
+        <Grid container justify="center" className={classes.root}
 
-            >
-                <Grid item style={{
-                    width: '50%',
-                    height: '100%',
-                    backgroundImage: `url(${Background})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: '100% 100%'
-                }} ></Grid>
-                <Grid container style={{ width: '50%', height: '100%', alignItems: 'center' }} justify="center" direction="column">
-                    <Grid item fullWidth style={{
-                        height: '20%',
-                        textAlign: 'center',
-                        fontSize: '3rem',
-                        fontWeight: 'bolder',
-                        color: '#f50057'
+        >
+            <Grid item style={{
+                width: '50%',
+                height: '100%',
+                backgroundImage: `url(${Background})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '100% 100%'
+            }} ></Grid>
+            <Grid container style={{ width: '50%', height: '100%', alignItems: 'center' }} justify="center" direction="column">
+                <Grid item fullWidth style={{
+                    height: '20%',
+                    textAlign: 'center',
+                    fontSize: '3rem',
+                    fontWeight: 'bolder',
+                    color: '#f50057'
 
-                    }}>
-                        MoviesOMDB
-                    </Grid>
-                    <Grid item style={{ width: '50%', justifyContent: 'center', display: 'flex', marginBottom: '10px' }}>
-                        <Button fullWidth variant="contained" color="primary" onClick={() => { setLogin(true) }} >Login</Button>
-                    </Grid>
-                    <Grid item style={{ width: '50%', justifyContent: 'center', display: 'flex' }}>
-                        <Button fullWidth variant="outlined" color="primary" onClick={() => { setSignup(true) }} >Signup</Button>
-                    </Grid>
+                }}>
+                    MoviesOMDB
+                </Grid>
+                <Grid item style={{ width: '50%', justifyContent: 'center', display: 'flex', marginBottom: '10px' }}>
+                    <Button fullWidth variant="contained" color="primary" onClick={() => { setLogin(true) }} >Login</Button>
+                </Grid>
+                <Grid item style={{ width: '50%', justifyContent: 'center', display: 'flex' }}>
+                    <Button fullWidth variant="outlined" color="primary" onClick={() => { setSignup(true) }} >Signup</Button>
                 </Grid>
             </Grid>
+        </Grid>
 
-            {
-                login ? (
-                    <FormModal handleClose={() => { setLogin(false) }} >
-                        <Login />
+        {
+            login ? (
+                <FormModal handleClose={() => { setLogin(false) }} >
+                    <Login />
+                </FormModal>
+            )
+                :
+                null
+        }
+        {
+            signup ?
+                (
+                    <FormModal handleClose={() => { setSignup(false) }} >
+                        <Signup />
                     </FormModal>
                 )
-                    :
-                    null
-            }
-            {
-                signup ?
-                    (
-                        <FormModal handleClose={() => { setSignup(false) }} >
-                            <Signup />
-                        </FormModal>
-                    )
-                    :
-                    null
+                :
+                null
 
-            }
-        </>
+        }
+    </>
     )
 
+    if (props.isAuthenticated) {
+        return <Redirect to="/home" />
+    } else {
+        return landing
+    }
 }
 
-export default LandingPage
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.isAuthenticated
+    }
+}
+
+export default connect(mapStateToProps)(LandingPage)
